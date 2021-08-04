20b35f6f1657c62af8ca862ccced9fcceb47af9d
Kamil Paprocki <kamil.paprocki1@gmail.com>
2021-08-04 20:37:46 +0200

    add CRUD in carFrontController add CRUD in CarFrontController

--- src/main/java/car_rental/api/car/CarFrontController.java
+++ src/main/java/car_rental/api/car/CarFrontController.java
@@ -2,9 +2,7 @@ package car_rental.api.car;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class CarFrontController {
@@ -16,16 +14,69 @@ private final CarService carService;
     }
 
     @GetMapping("/car")
-    public String carPage(Model model){
-        model.addAttribute("car", new Car());
-        return "form-car";
+    public String carPage(Model model,
+                          @RequestParam(required = false) String id,
+                          @RequestParam(required = false) String cars){
+
+        if ("carId".equals(cars)){
+            model.addAttribute("carById", carService.getCarById(Long.parseLong(id)));
+        }
+        if ("allCars".equals(cars)){
+            model.addAttribute("allCars", carService.getAllCars());
+        }
+        if ("availableCars".equals(cars)){
+            model.addAttribute("availableCars", carService.getAvailableCar());
+        }
+        if ("unavailableCars".equals(cars)){
+            model.addAttribute("unavailableCars", carService.getUnavailableCars());
+        }
+        return "cars";
+    }
+
+    @GetMapping("/add/car")
+    public String getAddCarPage(Model model){
+       model.addAttribute("car", new Car());
+        return "add-form-car";
     }
 
     @PostMapping("/add/car")
-    public String addCar(@ModelAttribute Car car, Model model){
-        model.addAttribute("car", car);
-        System.out.println(car.toString());
+    public String addCar(@ModelAttribute Car car){
         carService.createOrUpdateCar(car);
-        return "redirect:/car";
+        return "redirect:/home?info=added";
     }
+
+    @GetMapping("/update/edit-car")
+    public String getUpdateCar(){
+        return "update-car";
+    }
+
+    @GetMapping("/update/car")
+    public String getUpdateCarPage(@RequestParam Long id, Model model){
+        model.addAttribute("carById", carService.getCarById(id));
+        return "update-form-car";
+    }
+
+    @PostMapping("/update/car")
+    public String updateCar(@ModelAttribute("carById") Car carById){
+        carService.createOrUpdateCar(carById);
+        return "redirect:/home?info=updated";
+    }
+
+    @GetMapping("/cars")
+    public String getCarsPage(){
+        return "get-car";
+    }
+
+    @GetMapping("/delete/delete-car")
+    public String getDeleteCar(){
+        return "delete-car";
+    }
+
+    @GetMapping("/delete/car")
+    public String deleteCarPage(@RequestParam(required = false) Long id){
+           carService.deleteCarById(id);
+       return "redirect:/home?info=deleted";
+    }
+
+
 }
--- src/main/java/car_rental/api/utils/HomeFrontController.java
+++ src/main/java/car_rental/api/utils/HomeFrontController.java
@@ -1,13 +1,16 @@
 package car_rental.api.utils;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeFrontController {
 
     @GetMapping("/home")
-    public String getHomePage(){
+    public String getHomePage(Model model, @RequestParam(required = false) String info){
+        model.addAttribute("info", info);
         return "index";
     }
 }
