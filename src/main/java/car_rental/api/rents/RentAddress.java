aa3a94705398e6797f27b12487edbcd1d8e46acb
Kamil Paprocki <kamil.paprocki1@gmail.com>
2021-08-10 08:58:25 +0200

    remove lombok import

--- src/main/java/car_rental/api/rents/RentAddress.java
+++ src/main/java/car_rental/api/rents/RentAddress.java
@@ -1,10 +1,9 @@
 package car_rental.api.rents;
 
-import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 
 
-@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "rent_address")
 public class RentAddress extends Address {
