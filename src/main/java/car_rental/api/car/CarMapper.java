f607ebcf5749ce4b7fab5a4f0981d5cb713464d1
Kamil Paprocki <kamil.paprocki1@gmail.com>
2021-08-17 20:01:10 +0200

    add mapping from CarDTO to Car in CarMapper

--- src/main/java/car_rental/api/car/CarMapper.java
+++ src/main/java/car_rental/api/car/CarMapper.java
@@ -2,9 +2,11 @@ package car_rental.api.car;
 
 import car_rental.api.utils.DTOMapper;
 
+import java.math.BigDecimal;
+
 public class CarMapper implements DTOMapper<Car, CarDTO> {
     @Override
-    public CarDTO from(Car from) {
+    public CarDTO map(Car from) {
         return CarDTO.builder()
                 .brand(from.getBrand())
                 .model(from.getModel())
@@ -13,5 +15,18 @@ public class CarMapper implements DTOMapper<Car, CarDTO> {
                 .currentOdemeter(String.valueOf(from.getCurrentOdometer()))
                 .pricePerDay(from.getPricePerDay().toString())
                 .build();
+    }
+
+    @Override
+    public Car reverse(CarDTO from) {
+        Car car = new Car();
+        car.setBrand(from.getBrand());
+        car.setModel(from.getModel());
+        car.setSegment(CarSegment.valueOf(from.getSegment()));
+        car.setModelYear(Integer.parseInt(from.getModelYear()));
+        car.setCurrentOdometer(Long.parseLong(from.getCurrentOdemeter()));
+        car.setPricePerDay(new BigDecimal(from.getPricePerDay()));
+        car.setAvailable(true);
+        return car;
     }
 }
