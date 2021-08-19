09462561791444799caa7c6ba4001197a17381b7
Kamil Paprocki <kamil.paprocki1@gmail.com>
2021-08-19 17:48:25 +0200

    add log in and log out  user

---
+++ src/main/java/car_rental/api/user/UserLoginFrontController.java
@@ -0,0 +1,38 @@
+package car_rental.api.user;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class UserLoginFrontController {
+
+    CustomUserDetailsService customUserDetailsService;
+
+    public UserLoginFrontController(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @GetMapping("/login")
+    public String loginView(Model model, @RequestParam(required = false) String info){
+        model.addAttribute("info", info);
+        return "login";
+}
+
+    @GetMapping("/logout")
+    public String logoutView(HttpServletRequest request, HttpServletResponse response){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/login?info=logout";
+    }
+
+}
