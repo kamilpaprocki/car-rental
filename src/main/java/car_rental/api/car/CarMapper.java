db72410a2ffc3753a59d6aafdd65fcce1c52e652
Kamil Paprocki <kamil.paprocki1@gmail.com>
2021-08-31 12:53:36 +0200

    add Id and available to CarDTO and rebuild CarMapper

--- src/main/java/car_rental/api/car/CarMapper.java
+++ src/main/java/car_rental/api/car/CarMapper.java
@@ -8,11 +8,12 @@ public class CarMapper implements DTOMapper<Car, CarDTO> {
     @Override
     public CarDTO map(Car from) {
         return CarDTO.builder()
+                .id(from.getId().toString())
                 .brand(from.getBrand())
                 .model(from.getModel())
                 .segment(from.getSegment().toString())
                 .modelYear(String.valueOf(from.getModelYear()))
-                .currentOdemeter(String.valueOf(from.getCurrentOdometer()))
+                .currentOdometer(String.valueOf(from.getCurrentOdometer()))
                 .pricePerDay(from.getPricePerDay().toString())
                 .build();
     }
@@ -20,13 +21,14 @@ public class CarMapper implements DTOMapper<Car, CarDTO> {
     @Override
     public Car reverse(CarDTO from) {
         Car car = new Car();
+        car.setId(Long.parseLong(from.getId()));
         car.setBrand(from.getBrand());
         car.setModel(from.getModel());
         car.setSegment(CarSegment.valueOf(from.getSegment()));
         car.setModelYear(Integer.parseInt(from.getModelYear()));
-        car.setCurrentOdometer(Long.parseLong(from.getCurrentOdemeter()));
+        car.setCurrentOdometer(Long.parseLong(from.getCurrentOdometer()));
         car.setPricePerDay(new BigDecimal(from.getPricePerDay()));
-        car.setAvailable(true);
+        car.setAvailable(from.isAvailable());
         return car;
     }
 }
